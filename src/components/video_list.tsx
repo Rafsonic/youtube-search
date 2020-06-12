dacb673fd307a3602cdd84ecddee1207ec9c3f94
rafsonic <raf-ael@hotmail.com>
2020-06-12 10:02:21 +0300

    Update

--- src/components/video_list.tsx
+++ src/components/video_list.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import VideoListItem from "./video_list_item";
 import  "../styles/video-list.scss";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ReduxState } from "src/redux/reducers";
+import { selectedVideo } from "../redux/actions";
 
 const VideoList = () => {
+  const dispatch = useDispatch();
+
   const videos = useSelector<ReduxState, any[]>(({ videos }) => videos.videos);
   const videoItems = videos.map((video) => {
     return <VideoListItem key={video.etag} video={video} />;
   });
+
+  dispatch(selectedVideo(videos[0]));
 
   return <ul className="col-md-4 list-group video-list">{videoItems}</ul>;
 };
